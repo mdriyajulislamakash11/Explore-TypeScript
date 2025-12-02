b9989bfb3e953cef4e6a92be14309d04df2da464
Rj Akash Chowdury <hafejmohammadakash@gmail.com>
2025-12-02 22:22:39 +0600

    type thik rakhte hobe reassign korle jodi declare kora somoy value assign kore

--- output/index.js
+++ output/index.js
@@ -1,4 +1,13 @@
+// video-2
 const country = "Bangladesh new bangladesh hello ";
 console.log(country);
+// video-3 type thik rakhte hobe reassign korle
+let playerName = "A+J";
+let age = 22;
+playerName = 33;
+age = "ssd"; // error dibe  editor  a but value dekhabe thik thak
+console.log(playerName);
+console.log(playerName);
+console.log(age);
 export {};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-const country = "Bangladesh new bangladesh hello ";
 
+// video-2
+const country = "Bangladesh new bangladesh hello ";
 console.log(country);
+
+// video-3 type thik rakhte hobe reassign korle
+let playerName = "A+J"
+let age = 22;
+
+playerName = 33;
+age= "ssd" // error dibe  editor  a but value dekhabe thik thak
+
+console.log(playerName);
+console.log(playerName);
+console.log(age);
+
